ff1f2fdc7336c22b83b015b0428dba8080b7c426
mayaryasser2005 <mayaryasser300@gmail.com>
2024-08-29 12:07:18 +0300

    Initial commit.

---
+++ android/app/src/main/kotlin/com/route/news_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.route.news_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/api_manager.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'data/model/SourcesResponse.dart';
+
+
+
+
+class ApiManager{
+  //https://newsapi.org/v2/top-headlines/sources?apiKey=3f05df5cb5d043a2bab2d335179588ca&category=sports
+
+
+   Future<SourcesResponse>  getSources()async{
+    Uri url = Uri.https("newsapi.org","/v2/top-headlines/sources",{
+      "apiKey":"3f05df5cb5d043a2bab2d335179588ca"
+    });
+
+   http.Response response = await http.get( url);
+
+   var json = jsonDecode(response.body);
+
+    SourcesResponse sourcesRespones = SourcesResponse.fromJson(json);
+
+    return sourcesRespones;
+
+  }
+}
